cb7c85679e23282c0fb4679ef3945da5d7da8be9
Taís Foltran Orsini <taisfoltranorsini.com>
2021-07-08 14:32:09 -0300

    solved merge conflicts in EditModal, Days.js and App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,16 +4,11 @@ import "./App.css";
 import Agenda from "../Agenda/Agenda";
 import "../Days/Days.css";
 
-
 function App() {
   return (
     <BrowserRouter>
       <Route path="/" component={Agenda} />
-<<<<<<< HEAD
       {/* <Route path="/delete-eventDay/:id" component={DeleteEventDay} /> */}
-=======
-    
->>>>>>> b4e4bc1ee4899938c2cfbaf50901f11c76f13e2f
     </BrowserRouter>
   );
 }
